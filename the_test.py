034eba110e2fa9a8277545c92328b9a7c7f6b4d7
Russell Brand <brand@responsible.com>
2025-02-22 09:47:24 -0800

    passed test

--- the_test.py
+++ the_test.py
@@ -1,9 +1,13 @@
-import pytest
+# import pytest
 from dag import is_tree, Tree
 
 EMPTY_TREE = Tree([], [], [], {})
 
 
-def test_empty_tree_is_not_a_tree():
-
+def test_empty_graph_is_not_a_tree():
     assert is_tree(EMPTY_TREE) is False
+
+
+def test_graph_win_exactly_one_root_is_a_tree():
+    tree = Tree(['a'], [], ['a'], {})
+    assert is_tree(tree) is True
